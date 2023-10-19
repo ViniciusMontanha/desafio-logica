499acef7410e6797ed17170cc8e5982df773f500
ViniciusMontanha <viniciusadm@icloud.com>
2023-10-19 11:12:44 -0300

    salvar

---
+++ Desafio Lilpao/desafio.js
@@ -0,0 +1,14 @@
+let nomeHeroi = "Vinicius Monstro"
+let xpHeroi = 5000
+Let tomouPocao = true
+let novoXP = 
+if(tomouPocao){novoXP=7000
+}
+
+
+
+
+
+console.log (xpHeroi)
+console.log (nomeHeroi)
+
