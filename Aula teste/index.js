e8e1b453ca9651c53b0a6fab971b3e79e2d6b40a
ViniciusMontanha <viniciusadm@icloud.com>
2023-10-19 11:27:30 -0300

    Vamos que vamos!

--- Aula teste/index.js
+++ Aula teste/index.js
@@ -1,9 +1,39 @@
-console.log("ola mundao")
-console.log("ola")
+let nomeHeroi = " Vinicius Monstro "
+let xpHeroi = 10000
+let sofreuDano = false
+let nivel = ""
+let novoXP = ""
+    if(sofreuDano){novoXP=8000
+}
+    else{("Enfrentou boss Final ")
+novoXP=6995
+}
+console.log(novoXP)
+let xpFinal =  6995
+for(let i= 6995;i<7015;i++){
+  xpFinal+=1
+  console.log("tomou poção magica " + i)
+}
+console.log( xpFinal + " totais ")
+if(let = xpFinal<=1000){
+    console.log("ferro")}
+    if(let = xpFinal>=1001 && xpFinal<=2000 ){
+        console.log("Bronze")}
+        if(let = xpFinal>=2001 && xpFinal<=5000 ){
+            console.log("Prata")}
+            if(let = xpFinal>=6001 && xpFinal<=7001 ){
+                console.log("Ouro")}
+                if(let = xpFinal>=7001 && xpFinal<=8000 ){
+                    console.log("platina")}
+                    if(let = xpFinal>=8001 && xpFinal<=9000 ){
+                        console.log("Ascendente")}
+                        if(let = xpFinal>=9001 && xpFinal<=1000 ){
+                            console.log("Imortal")}
+                            if(let = xpFinal>=10001){
+                                console.log("Radiante")}
 
-
-Text
-
-TextDecoderStream
-
-TextDecoderStream
+let platina = true
+if(platina){
+    nivel = " platina"
+}
+console.log ("O heroi" + nomeHeroi + "esta no nivel"+ nivel)
